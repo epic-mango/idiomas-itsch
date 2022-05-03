06cced88c8218671c1a81eb62701f1960a99d574
Juan Daniel Rodríguez Espinoza <drany@live.com>
2022-05-03 02:38:40 -0500

    Se redujo el codigo de la busqueda de correos

--- app/Http/Livewire/BusquedaCorreo.php
+++ app/Http/Livewire/BusquedaCorreo.php
@@ -7,29 +7,32 @@ use Livewire\Component;
 
 class BusquedaCorreo extends Component
 {
+    //Variables públicas que pueden ser accedidas desde la vista
     public $busqueda = '';
     public $seleccionado;
     public $identificador;
     public $resultado = [];
 
+    //función que se ejecuta al modificar el valor de la variable $busqueda
     public function updatedBusqueda($value)
     {
+        //si había un correo seleccionado, se limpia el valor de $busqueda para no confundir al usuario
         if($this->seleccionado){
             $this->busqueda = '';            
-        }           
+        }     
+        
+        //Siempre que se modifica un valor de la variable $busqueda, se limpia la variable $seleccionado e $identificador 
+        //y se inicia una nueva búsqueda
         $this->seleccionado = false;
         $this->identificador = null;
-    }
 
-    public function buscar()
-    {
         if (strlen($this->busqueda) > 0) {
+            //Además de buscar usuarios que coincidan con la entrada del usuario, se buscan usuarios que coincidan con el rol 'Alum'
             $this->resultado = User::where('email', 'like', '%' . $this->busqueda . '%')->role('Alum')->get();
         }
-     
-
     }
 
+    //función que se ejecuta al seleccionar un correo
     public function seleccionar($identificador, $email)
     {
         $this->seleccionado = true;
--- resources/views/livewire/busqueda-correo.blade.php
+++ resources/views/livewire/busqueda-correo.blade.php
@@ -2,12 +2,11 @@
     <input maxlength="30" type="hidden" id="buscaremail" name="ADMIN_CORREO" value="{{$identificador}}" required>
     {!! $errors->first('ADMIN_CORREO', '<spanclass="alert-danger">:message</span><br>') !!}
     <input type="text" class="form-control {{ $seleccionado ? 'is-valid' : 'is-invalid' }}" id="busqueda"
-        wire:model="busqueda" wire:keydown="buscar" autocomplete="off" required />
+        wire:model="busqueda" autocomplete="off" placeholder="ejemplo@itsch.edu.mx" required />
     <div class="form-group ajax-search">
         <ul id="resultados" class="dropdown-menu @if (sizeof($resultado) > 0 && !$seleccionado) show @endif ">
             @foreach ($resultado as $res)
-                <li class="dropdown-item" onclick="$('#resultados').removeClass('show');"
-                    wire:click="seleccionar({{ $res['id'] }}, '{{ $res['email'] }}')">{{ $res['email'] }}</li>
+                <li class="dropdown-item" wire:click="seleccionar({{ $res['id'] }}, '{{ $res['email'] }}')">{{ $res['email'] }}</li>
             @endforeach
         </ul>
     </div>
